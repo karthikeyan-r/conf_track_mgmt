8676e944b4e662b220b5744822e9ba009c70bb8b
Karthikeyan R <karthikeyaneie.kln@gmail.com>
2017-02-18 16:35:55 +0530

    Common Writer for Console/File

--- src/main/java/com/ctm/io/CTMFileWriter.java
+++ src/main/java/com/ctm/io/CTMFileWriter.java
@@ -1,7 +1,9 @@
 package com.ctm.io;
 
 import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.util.List;
 
 import com.ctm.exception.CTMException;
@@ -13,16 +15,32 @@ public class CTMFileWriter {
 
 	private List<Session> sessionLst = null;
 
+	BufferedWriter logWriter = null;
+
 	public CTMFileWriter(String fileNm, List<Session> sessionLst) {
 		this.fileName = fileNm;
 		this.sessionLst = sessionLst;
+		try {
+			if (this.fileName != null)
+				logWriter = new BufferedWriter(new PrintWriter(this.fileName));
+			else
+				logWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public CTMFileWriter(List<Session> sessionLst) {
+		this(null, sessionLst);
 	}
 
 	public void writeFile() throws CTMException {
-		try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileName))) {
-			// Read File Line By Line
-			for (Session session : sessionLst) {
-				// WRITE_TO_FILE
+		try {
+			if (logWriter != null) {
+				for (Session session : sessionLst) {
+					logWriter.write("");
+				}
 			}
 		} catch (Exception e) {// Catch exception if any
 			e.printStackTrace();
