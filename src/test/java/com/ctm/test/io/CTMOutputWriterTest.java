d7a6bd31a3c22ca4419f68624ee7168a78cad20a
karthikeyan-r <karthikeyaneie.kln@gmail.com>
2017-02-20 15:07:31 +0530

    Create CTMOutputWriterTest.java

---
+++ src/test/java/com/ctm/test/io/CTMOutputWriterTest.java
@@ -0,0 +1,11 @@
+package com.ctm.test.io;
+
+import junit.framework.TestCase;
+
+public class CTMOutputWriterTest extends TestCase {
+
+	public void testWriteFile() {
+		fail("Not yet implemented");
+	}
+
+}
