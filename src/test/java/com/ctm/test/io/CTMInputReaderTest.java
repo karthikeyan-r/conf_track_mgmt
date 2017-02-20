692d402a9e8e3abdc7ff318eb26e62c2e67a5c9c
karthikeyan-r <karthikeyaneie.kln@gmail.com>
2017-02-20 15:07:12 +0530

    Create CTMInputReaderTest.java

---
+++ src/test/java/com/ctm/test/io/CTMInputReaderTest.java
@@ -0,0 +1,23 @@
+package com.ctm.test.io;
+
+import junit.framework.TestCase;
+
+public class CTMInputReaderTest extends TestCase {
+
+	public void testGetPresentationList() {
+		fail("Not yet implemented");
+	}
+
+	public void testIsValidInputLine() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetMaxPresentationTime() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetMaxPresentationTime() {
+		fail("Not yet implemented");
+	}
+
+}
