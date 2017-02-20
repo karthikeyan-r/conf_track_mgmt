c9b3a57f9500f323b5a63256355ab585efb28bed
karthikeyan-r <karthikeyaneie.kln@gmail.com>
2017-02-20 15:06:40 +0530

    Create CTMSchedulerTest.java

---
+++ src/test/java/com/ctm/test/scheduler/CTMSchedulerTest.java
@@ -0,0 +1,11 @@
+package com.ctm.test.scheduler;
+
+import junit.framework.TestCase;
+
+public class CTMSchedulerTest extends TestCase {
+
+	public void testSplitNSegment() {
+		fail("Not yet implemented");
+	}
+
+}
