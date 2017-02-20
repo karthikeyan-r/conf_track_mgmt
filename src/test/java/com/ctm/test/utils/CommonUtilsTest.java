c99adfb54d932d05890370487ff5753cf9761658
karthikeyan-r <karthikeyaneie.kln@gmail.com>
2017-02-20 15:06:03 +0530

    Create CommonUtilsTest.java

---
+++ src/test/java/com/ctm/test/utils/CommonUtilsTest.java
@@ -0,0 +1,19 @@
+package com.ctm.test.utils;
+
+import junit.framework.TestCase;
+
+public class CommonUtilsTest extends TestCase {
+
+	public void testGetFormattedTime() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetFormattedTimeStr() {
+		fail("Not yet implemented");
+	}
+
+	public void testAddMinutes() {
+		fail("Not yet implemented");
+	}
+
+}
