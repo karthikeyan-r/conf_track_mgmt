5cfd465cd280e15d1a27d397fdcfef36002bcb19
karthikeyan-r <karthikeyaneie.kln@gmail.com>
2017-02-20 15:05:46 +0530

    Create StringUtilsTest.java

---
+++ src/test/java/com/ctm/test/utils/StringUtilsTest.java
@@ -0,0 +1,23 @@
+package com.ctm.test.utils;
+
+import junit.framework.TestCase;
+
+public class StringUtilsTest extends TestCase {
+
+	public void testIsNotNullNotEmpty() {
+		fail("Not yet implemented");
+	}
+
+	public void testParseInt() {
+		fail("Not yet implemented");
+	}
+
+	public void testTrimExtraSpaces() {
+		fail("Not yet implemented");
+	}
+
+	public void testTryParseInt() {
+		fail("Not yet implemented");
+	}
+
+}
